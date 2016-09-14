5f85d91ef0f788a7cafd8a6cea933136b9b98560
Mate Verunica <verunica@cs.ualberta.ca>
2016-09-14 14:23:00 -0600

    google socket connect

---
+++ lab2.py
@@ -0,0 +1,24 @@
+import socket
+
+#Allocate a new socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Connect to google.ca
+
+client.connect(('www.google.ca',80))
+
+http = "GET / HTTP/1.0\r\n\r\n"
+
+client.sendall(http)
+
+msg = ""
+while True:
+    part = client.recv(1024)
+    if part:
+        msg += part 
+    else:
+        break
+
+print msg
+
